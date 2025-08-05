e38a10406718d009f61922cd4b03ce2fbd80907a
BlackJVictor <joao.fiel.2014@hotmail.com>
2025-08-05 20:19:22 -0300

    Projeto parte 1

---
+++ config/connectDB.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+if(!process.env.MONGODB_URI) {
+    throw new Error('ESTA STRING DE CONEXÃO DO BANCO DE DADOS NAO ESTA NO ARQUIVO .ENV');
+}
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,);
+        console.log('Conectado ao banco de dados com sucesso');
+    } catch (error) {
+        console.log('Erro ao conectar ao banco de dados', error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
